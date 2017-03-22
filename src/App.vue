3f7834ef638ca5b62d3a5c59b9198f989b57fe69
mike connor <mike@rocketnumber9.org>
2017-03-22 15:52:34 -0500

    rebuild with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,277 @@
+<template>
+<div id="app">
+  <div class="card">
+      <header class="card-header">
+            <p class="card-header-title">
+                  {{ msg }}
+            </p>
+      </header>
+      <div class="card-content">
+            <main :class='mainClass'>
+                  <bulb type='incandescent' :klass='inc' :wattage='inc_wattage' :tcost='inc_tcost'></bulb>
+                  <bulb type='halogen' :klass='hal' :wattage='hal_wattage' :tcost='hal_tcost'></bulb>
+                  <bulb type='cfl' :klass='cfl' :wattage='cfl_wattage' :tcost='cfl_tcost'></bulb>
+                  <bulb type='led' :klass='led' :wattage='led_wattage' :tcost='led_tcost'></bulb>
+            </main>
+            <form>
+                  <div>
+                        <h4>Lumens</h4>
+                        <p>Brightness
+                              <br>of bulb</p>
+                        <!-- <span class="control"> -->
+                        <p class="select control">
+                              <select v-model.number='current_lumens'>
+                                    <option v-for="option in lumen_options" :value="option.val">
+                                          {{ option.val }}
+                                    </option>
+                              </select>
+                        </p>
+                        <!-- </span -->
+                  </div>
+                  <div>
+                        <h4>kWh</h4>
+                        <p>Kilowatt
+                              <br>-hours cost</p>
+                        <p class="control">
+                              <input class="input" type="number" v-model.number="current_cost" placeholder="current_cost">cents
+                        </p>
+                  </div>
+
+                  <div>
+                        <h4>Hours</h4>
+                        <p>Usage
+                              <br>per day</p>
+                        <p class="control">
+                              <input class="input" type="number" v-model.number="current_hours" placeholder="current_cost">
+                        </p>
+                  </div>
+            </form>
+      </div>
+      <footer class="card-footer">
+
+            <!-- <a class="card-footer-item">Save</a>
+           <a class="card-footer-item">Edit</a>
+           <a class="card-footer-item">Delete</a>
+          -->
+            <p class="card-footer-item">Made with <a href='https://vuejs.org/'>Vue.js</a></p>
+
+      </footer>
+</div>
+</div>
+</template>
+
+<script>
+import Bulb from './components/Bulb';
+
+export default {
+  name: 'app',
+  components: {
+    Bulb,
+  },
+  data() {
+    return {
+      total_days: 365,
+
+      inc_conv: 0.0625,
+      hal_conv: 0.045,
+      cfl_conv: 0.0146,
+      led_conv: 0.0125,
+
+      msg: 'Light Bulb Wattage Calculator App. Made with Vue.js',
+      inc: 'bg-inc',
+      hal: 'bg-hal',
+      cfl: 'bg-cfl',
+      led: 'bg-led',
+      current_cost: 21,
+      current_hours: 10,
+      mainClass: 'my-main',
+      lumen_options: [{
+        val: 375,
+      }, {
+        val: 600,
+      }, {
+        val: 900,
+      }, {
+        val: 1125,
+      }, {
+        val: 1600,
+      }],
+      current_lumens: 375,
+
+    };
+  },
+
+  computed: {
+    // wattage(conv) {
+    //   return (this.current_lumens * conv).toFixed(2);
+    // },
+
+    inc_wattage() {
+      return (this.current_lumens * this.inc_conv)
+        .toFixed(2);
+    },
+    hal_wattage() {
+      return (this.current_lumens * this.hal_conv)
+        .toFixed(2);
+    },
+    cfl_wattage() {
+      return (this.current_lumens * this.cfl_conv)
+        .toFixed(2);
+    },
+    led_wattage() {
+      return (this.current_lumens * this.led_conv)
+        .toFixed(2);
+    },
+
+    inc_tcost() {
+      // var inc_wattage = (this.current_lumens * this.inc_conv).toFixed(2);
+      // var inc_wattage = wattage(this.inc_conv);
+      return ((this.inc_wattage * this.total_hours * this.cost) / 1000)
+        .toFixed(2);
+    },
+    hal_tcost() {
+      // const hal_wattage = (this.current_lumens * this.hal_conv).toFixed(2);
+      // console.log('hal_wattage ' +this.hal_wattage);
+      return ((this.hal_wattage * this.total_hours * this.cost) / 1000)
+        .toFixed(2);
+    },
+    cfl_tcost() {
+      // const cfl_wattage = (this.current_lumens * this.cfl_conv).toFixed(2);
+      return ((this.cfl_wattage * this.total_hours * this.cost) / 1000)
+        .toFixed(2);
+    },
+    led_tcost() {
+      //  const led_wattage = (this.current_lumens * this.led_conv).toFixed(2);
+      return ((this.led_wattage * this.total_hours * this.cost) / 1000)
+        .toFixed(2);
+    },
+
+    total_hours() {
+      return this.total_days * this.current_hours;
+    },
+    cost() {
+      return this.current_cost / 100;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("../node_modules/bulma/css/bulma.css");
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:300,700|Open+Sans+Condensed:700);*/
+body {
+    background: #000;
+}
+#app {
+    margin: 0;
+    padding: 40;
+    font-size: 16px;
+    font-weight: 300;
+    background: #000;
+    color: #fff;
+}
+
+#app .card-header-title {
+    color: #fff;
+}
+.card {
+  background-color: #000;
+  color: #fff;
+}
+
+#app form input[type=number] {
+    width: 5em;
+    text-align: center;
+    font-size: 1em;
+    margin-right: 1em;
+}
+
+.control {
+    margin-top: 6px;
+    text-align: center;
+}
+
+.my-main {
+    background: brown;
+    display: flex;
+    justify-content: space-around;
+}
+
+.bg-inc,
+.bg-cfl,
+.bg-hal,
+.bg-led {
+    width: 25%;
+}
+
+.bg-inc {
+    background-color: #111;
+    background-image: url(./assets/bulb_inc.svg);
+}
+
+.bg-hal {
+    background-color: #444;
+    background-image: url(./assets/bulb_hal.svg);
+}
+
+.bg-cfl {
+    background-color: #777;
+    background-image: url(./assets/bulb_cfl.svg);
+}
+
+.bg-led {
+    background-color: #aaa;
+    background-image: url(./assets/bulb_led.svg);
+}
+
+#app form {
+    display: flex;
+    justify-content: space-around;
+}
+
+#app form select {
+    font-size: 1em;
+    /*-webkit-appearance: none;
+-moz-appearance: none;
+appearance: none;
+    background: #fff url(./assets/icons_arrows.svg) no-repeat right 6px;
+  background-size: 30px 20px;*/
+    padding: 3px 30px 3px 5px;
+}
+
+#app form h4 {
+    color: #aaa;
+    margin: 0 0 -5px 0;
+    font-size: 1.5em;
+    font-weight: 300;
+}
+
+#app form p {
+    font-size: .8em;
+    line-height: 14px;
+}
+
+#app form br {
+    display: none;
+}
+
+#app form>div {
+    width: 33%;
+    /*float: left;*/
+    text-align: center;
+    color: white;
+    /*padding: 10px 0 5px 0;*/
+}
+
+form::after {
+    content: "";
+    display: block;
+    clear: both;
+}
+
+@media screen and (max-width: 500px) {
+    form br {
+        display: inline;
+    }
+}
+</style>
---
+++ src/components/Bulb.vue
@@ -0,0 +1,125 @@
+<template>
+  <section :class='klass'>
+      <h4>{{type}}</h4>
+      <p>{{wattage}}</p>
+      <p>{{tcost}}</p>
+  </section>
+</template>
+
+<script >
+export default {
+  name: 'bulb',
+  props: {
+    klass: { required: true },
+    type: { required: true },
+    wattage: { required: true, default: '11' },
+    tcost: { required: true, default: '12' },
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+section {
+
+font-size: 1em;
+/*font-family: 'Open Sans', sans-serif;
+line-height: 24px;*/
+    font-weight: 300;
+
+
+    text-align: center;
+
+    padding-top: 110px;
+    background-position: center 5px;
+    background-size: 63px 100px;
+    background-repeat: no-repeat;
+
+}
+
+
+/* apply rule on first descendent div */
+section > div {
+    padding: 5px 0;
+    background-color: rgba(0, 0, 0, .15)
+}
+
+
+
+section h4 {
+    font-family: 'Open Sans Condensed', sans-serif;
+    font-size: .9em;
+    letter-spacing: 1px;
+    margin: 0 0 5px 0;
+    text-transform: uppercase;
+}
+
+section p {
+    margin: 0;
+    font-size: 1.5em;
+    font-weight: 700;
+}
+
+/*style applies to p followin another p*/
+section p + p {
+    font-size: 1em !important;
+    font-weight: 300;
+}
+
+section p::after {
+    content: 'w';
+}
+
+
+section p + p::after {
+    content: 'per year';
+    color: rgba(255, 255, 255, .5);
+    padding-left: 4px;
+}
+
+section p + p::before {
+    content: '$';
+}
+
+
+@media screen and (max-width: 500px) {
+    {
+        font-size: 15px;
+    }
+
+    section {
+        background-size: 45px 71px;
+        padding-top: 80px;
+    }
+
+    section h4 {
+      font-family: 'Open Sans Condensed', sans-serif;
+  font-size: .8em;
+  letter-spacing: 1px;
+  margin: 0 0 5px 0;
+  text-transform: uppercase;
+    }
+
+    section p {
+        font-size: 1.1em;
+        font-size: 1.5em;
+    font-weight: 700;
+    }
+
+    section p + p {
+        font-size: 1em;
+        font-weight: 300
+    }
+
+    section p + p::after {
+        display: block;
+        color: #00f;
+        margin-top: -10px;
+    }
+}
+</style>
